1a9d3c1251526f0dce4745d80d53252999bb9093
Malavika K <165053248+Malavika2506@users.noreply.github.com>
2025-07-28 21:32:01 +0530

    Travel Planner UI

---
+++ main.js
@@ -0,0 +1,43 @@
+// main.js
+
+// var swiper = new Swiper(".swiper", {
+// effect: "coverflow",
+// grabCursor: true,
+// centeredSlides: true,
+// slidesPerView: 2.75,
+// speed: 600,
+// coverflowEffect: {
+// rotate: 10,
+// stretch: 0,
+// depth: 100,
+// modifier: 3,
+// slideShadows: true,
+// },
+// loop: true,
+// pagination: {
+// el: ".swiper-pagination",
+// clickable: false,
+// },
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2.75,
+    speed: 600,
+    coverflowEffect: {
+      rotate: 10,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+      slideShadows: true,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+    },
+  });
+});
